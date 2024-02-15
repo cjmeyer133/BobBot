e65b5c8da060677403c7d2c2b4141bffef5c8c7a
cjmeyer133 <cjmeyer133@gmail.com>
2024-02-15 16:08:33 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,25 +4,48 @@
 import json, os, time, random, math, sys, discord, math
 from datetime import datetime, timezone, timedelta
 from random import randint
+
+# imports
+import discord, os, json, sys
+import random
+from discord.ext.commands import Bot
+# custom database handler
+import database
+from time import sleep
+
+from keep import alive
 #end from there
 
 #from examples in discord.py github
 import asyncio
 import logging
 import logging.handlers
-import os
 
 from typing import List, Optional
 
 import asyncpg  # asyncpg is not a dependency of the discord.py, and is only included here for illustrative purposes.
-import discord
 from discord.ext import commands
 from discord import app_commands
 from aiohttp import ClientSession
 #end from there
 
+alive()
 
-#wow Unbelieva-Boat is gross, here's some changes. Hopefully not omitting anything necessary
+# init discord stuff and json handling
+BOT_PREFIX = ("gg")
+BOT_PREFIX_LIST = ["gg"]
+token = os.environ["token"]
+# emojis
+emoji_worked = "✅"
+emoji_error = "❌"
+discord_error_rgb_code = discord.Color.from_rgb(239, 83, 80)
+intents = discord.Intents.all()
+client = Bot(command_prefix=BOT_PREFIX, intents=intents)  # init bot
+db_handler = database.pythonboat_database_handler(client)  # ("database.json")
+with open("owner.json") as f:
+    adminlist = json.load(f)
+
+#wow Unbelieva-Boat is gross, there's some changes in here. Hopefully not omitting anything necessary
 class pythondiscord_bot_handler:
      def find_index_in_db(self, data_to_search, user_to_find, fail_safe=False):
         print(data_to_search)
