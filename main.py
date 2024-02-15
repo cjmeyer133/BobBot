25804c330646acb3ecd3cb7eb6479267f7ce8e7b
cjmeyer133 <cjmeyer133@gmail.com>
2024-02-15 16:10:04 -0500

    created init which I think we need

--- main.py
+++ main.py
@@ -45,34 +45,7 @@ db_handler = database.pythonboat_database_handler(client)  # ("database.json")
 with open("owner.json") as f:
     adminlist = json.load(f)
 
-#wow Unbelieva-Boat is gross, there's some changes in here. Hopefully not omitting anything necessary
-class pythondiscord_bot_handler:
-     def find_index_in_db(self, data_to_search, user_to_find, fail_safe=False):
-        print(data_to_search)
-        user_to_find = int(user_to_find)
-        for i in range(len(data_to_search)):
-            if data_to_search[i]["user_id"] == user_to_find:
-                print("\nfound user\n")
-                return int(i), "none"
 
-        # in this case, this isnt a user which isnt yet registrated
-        # but someone who doesnt exist on the server
-        # or at least thats what is expected when calling with this parameter
-        if fail_safe:
-            return 0, "error"
 
-        print("\ncreating user\n")
-        # we did NOT find him, which means he doesn't exist yet
-        # so we automatically create him
-        data_to_search.append({
-            "user_id": int(user_to_find),
-            "cash": 0,
-            "bank": 0,
-        })
 
-        # now that the user is created, re-check and return int
-
-        for i in range(len(data_to_search)):
-            if data_to_search[i]["user_id"] == user_to_find:
-                return i, data_to_search
             
