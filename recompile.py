dc505b93a838dae90579ff787f3db9b3bd492bcf
cjmeyer133 <cjmeyer133@gmail.com>
2024-02-19 15:21:56 -0500

    Update recompile.py

--- recompile.py
+++ recompile.py
@@ -5,3 +5,15 @@ import json, os, time, random, math, sys, discord, math
 from datetime import datetime, timezone, timedelta
 from random import randint
 
+class pythondiscord_bot_handler:
+    def __init__(self, client):
+        # we do the path from the main.py file, so we go into the db folder, then select
+        self.pathToJson = "database/database.json"
+        self.client = client
+        # for the json "variables", dont want to make a whole function to find index for variables
+        # wont be many anyways. so making it manually
+        self.variable_dict = {
+            "daily": 0,}
+
+
+
