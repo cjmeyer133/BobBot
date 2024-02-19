bf084caed9fde14d571c3770779bea003c662478
cjmeyer133 <cjmeyer133@gmail.com>
2024-02-19 15:14:05 -0500

    Create recompile.py

---
+++ recompile.py
@@ -0,0 +1,7 @@
+#I'm putting things together again in a separate document being a bit more picky.
+#this particular one is for the init file
+
+import json, os, time, random, math, sys, discord, math
+from datetime import datetime, timezone, timedelta
+from random import randint
+
