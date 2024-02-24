40480a90fbb9d49aaa4364e69241aa15a4bc8ab0
cjmeyer133 <cjmeyer133@gmail.com>
2024-02-24 14:12:41 -0500

    database json + new init for bot?
    Checked to see if database.json already existing seemed to fix things (it didn't) and decided to just plop as much as possible into a single file

---
+++ recode-init.py
@@ -0,0 +1,54 @@
+#trying to get working commands/bot
+
+
+###########
+# IMPORTS #
+###########
+
+import os
+
+import discord
+import asyncio
+from dotenv import load_dotenv
+
+import json, time, random, math, sys
+from datetime import datetime, timezone, timedelta
+from random import randint
+
+import logging
+import logging.handlers
+
+from discord.ext.commands import Bot
+# custom database handler
+import database
+from time import sleep
+
+from keep import alive
+
+#############
+# GET TOKEN #
+#############
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+########################
+# INTENTS/CLIENT SETUP #
+########################
+
+intents = discord.Intents.all()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    #prints this when bot has connected to discord
+    print(f'{client.user} has connected to Discord!')
+
+
+#############
+# START BOT #
+#############
+
+client.run(TOKEN)
